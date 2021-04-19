58585e86144b0cc8b63097493dfbda7e32abe5a2
hoon2hooni <sanghoon.engineer@gmail.com>
2021-04-19 10:51:10 +0900

    Fix to show zero evenif time less than 10

--- clock/clock.js
+++ clock/clock.js
@@ -7,11 +7,16 @@ function getTime(){
     const minutes  = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds}`;
 }
+
 function init(){
     console.log("I am working");
     getTime();
+    setInterval(getTime, 1000);
 }
-"check"
+
 init()
