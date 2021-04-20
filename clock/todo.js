6239b4da52502b08265eb24cdb8c213086d76dfd
hoon2hooni <sanghoon.engineer@gmail.com>
2021-04-20 21:02:49 +0900

    Add function which can delete to-do list with clicking button

--- clock/todo.js
+++ clock/todo.js
@@ -3,7 +3,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos'
-const toDos = [];
+let toDos = [];
+function filterFn(todo){
+    return toDo.id === 1;
+}
+
+function deleteTodo(event){
+    console.log(event.target.parentElement);
+    const btn = event.target;
+    const li = btn.parentElement;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos(toDos);
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -15,13 +30,13 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerHTML = "button";
+    delBtn.addEventListener("click",deleteTodo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
     // li에도 id부여함
     li.id = newId;
     toDoList.appendChild(li);
-    console.log(text);
     const toDoObj ={
         text: text,
         id: newId
@@ -44,7 +59,6 @@ function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
     if (loadedToDos !== null){
         const parsedToDos = JSON.parse(loadedToDos)
-        console.log(parsedToDos)
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         });
@@ -53,6 +67,5 @@ function loadToDos(){
 function init(){
     loadToDos();
     toDoForm.addEventListener("submit", handleSubmit);
-    console.log(toDoInput.value);
 }
 init();
