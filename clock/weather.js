4d57d07bcd8c1607a22e0ba9f80eacb6e21512da
hoon2hooni <sanghoon.engineer@gmail.com>
2021-04-27 19:52:43 +0900

    Apply openweathermap API to get current weather at current location

--- clock/weather.js
+++ clock/weather.js
@@ -1,6 +1,19 @@
+
+const weather = document.querySelector(".js-weather");
+
 const COORDS = 'coords';
-
-
+const API_KEY = "9f8fa7deb4f0c3dde06c6796cd466596";
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    ).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 // function paintCooard(){
 //     document.createElement();
 //     document.createElement();
@@ -11,6 +24,7 @@ function saveCoords(coordsObjs){
     localStorage.setItem("longitude", JSON.stringify(coordsObjs.longitude));
 }
 
+
 function handleGeoSucces(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -19,6 +33,7 @@ function handleGeoSucces(position){
         longitude,
     }
     saveCoords(coordsObjs);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -35,10 +50,12 @@ function loadCoords(){
         askForCoords();
         // localStorage.setItem blah;
     } else {
-        coordsObjs = JSON.parse(loadedCoords);
-        latitude = coordsObjs.latitude;
-        longitude = coordsObjs.longitude;
-        console.log(loadedCoords);
+        const parseCoords = JSON.parse(loadedCoords);
+        latitude = parseCoords.latitude;
+        longitude = parseCoords.longitude;
+        console.log(latitude);
+        console.log(API_KEY);
+        getWeather(latitude.longitude);
         // paintCooard();
     }
 }
